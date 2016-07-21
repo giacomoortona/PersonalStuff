d46e441169b2ab8227a2bcb7f755e6c035b449e9
giacomoortona <gortona@cern.ch>
2016-07-21 18:17:28 +0200

    Create submitRVRF.sh

---
+++ ZZ4l/submitRVRF.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+First=$1
+Last=$2
+
+cd ${LS_SUBCWD}
+
+echo "LSF job running in: " `pwd` with options $First $Last
+
+eval `scram runtime -sh`
+
+combine -M MultiDimFit comb_hzz4l.root --algo=grid --points 1000 -m 125.09 -n RVRF_nLL_scan_${First} --firstPoint $First --lastPoint $Last -v 1 --setPhysicsModelParameterRanges RV=0,5:RF0:5
